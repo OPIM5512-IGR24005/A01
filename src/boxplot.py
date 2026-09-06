163a85c8da385d19eb48097a20b5fada0c900139
Christie Amoah <igr24005@uconn.edu>
2026-09-06 17:20:28 -0400

    Add CA housing boxplot script and output figure

--- src/boxplot.py
+++ src/boxplot.py
@@ -1,5 +1,6 @@
 from sklearn.datasets import fetch_california_housing
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # Load California Housing dataset
 housing = fetch_california_housing(as_frame=True)
@@ -10,3 +11,13 @@ df = housing.frame
 # Quick check
 print(df.head())
 print(df.shape)
+
+fig, ax = plt.subplots(figsize=(10, 6))
+ax.boxplot(df["MedHouseVal"], vert=True, widths=0.5)
+ax.set_title("MedHouseVal")
+ax.set_xticks([])
+plt.suptitle("Boxplot for California Housing Dataset", fontsize=12)
+plt.tight_layout()
+
+plt.savefig("figs/boxplot.png")
+plt.show()
